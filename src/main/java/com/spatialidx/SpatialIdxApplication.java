9207d91383840ad836525a2d7a86e4d745985a84
LSH <lsh2613@naver.com>
2024-11-20 23:44:52 +0900

    [build] 초기 세팅 + hibernate-spatial 의존성 추가

---
+++ src/main/java/com/spatialidx/SpatialIdxApplication.java
@@ -0,0 +1,13 @@
+package com.spatialidx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpatialIdxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpatialIdxApplication.class, args);
+    }
+
+}
